ed085ac6f15f93c0420ff3e82934f9fb8a0be85b
danaohayon <dandaohayon@gmail.com>
2024-04-01 11:14:06 +0300

    Initial Commit

---
+++ DogLocalizerSUI/DogLocalizerSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogLocalizerSUIApp.swift
+//  DogLocalizerSUI
+//
+//  Created by Dana Ohayon on 01/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct DogLocalizerSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DogLocalizerSUIUITests/DogLocalizerSUIUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  DogLocalizerSUIUITestsLaunchTests.swift
+//  DogLocalizerSUIUITests
+//
+//  Created by Dana Ohayon on 01/04/2024.
+//
+
+import XCTest
+
+final class DogLocalizerSUIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
