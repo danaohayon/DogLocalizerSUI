e9d2da442894e36f626cb84094f22e8560516f5e
danaohayon <dandaohayon@gmail.com>
2024-04-05 01:04:22 +0300

    maybe added a favorite button (preview crashed). anyway if works can only chyange the star to full or empty,doesn't favorite anything

--- DogLocalizerSUI/ContentView.swift
+++ DogLocalizerSUI/ContentView.swift
@@ -8,15 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var imageName:String = "star";
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+                Button(action: {
+                    favorite()
+                }, label: {
+                    Image(systemName: imageName)
+                })
         }
         .padding()
+        
+        
     }
+    
+    func favorite(){
+        // if the star is empty, fill when pressed. if full, empty.
+        //TODO: have an pbject to favorite and so on
+        if (imageName == "star") {
+            imageName = "star.fill"
+        } else {
+            imageName = "star"
+        }
+    }
+    
 }
 
 #Preview {
