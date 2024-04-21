236045b56b63d1d4c4eed7a4dcab5444548258e1
danaohayon <dandaohayon@gmail.com>
2024-04-21 21:06:11 +0300

    created a home page UI, with 4 image view and basically every element i need but without functionality

---
+++ DogLocalizerSUI/HomePage.swift
@@ -0,0 +1,108 @@
+import SwiftUI
+
+struct HomePage: View {
+    
+    let trebuchetMS: Font = Font.custom("TrebuchetMS", size: 16)
+    
+    var body: some View {
+        ZStack(alignment: .top) {
+
+            Color("panelColor")
+                .edgesIgnoringSafeArea(.all)
+            
+            HStack {
+                Image(systemName: "line.3.horizontal")
+                Spacer()
+                Text("Made by Dana Ohayon")
+                    .font(.custom("TrebuchetMS", size: 10))
+                    .padding(.vertical, 5)
+                    .foregroundColor(Color("AccentColor").opacity(0.7))
+                Spacer()
+                Image(systemName: "questionmark.circle")
+            }.padding(.horizontal, 35)
+                .foregroundColor(Color("AccentColor"))
+                .fontWeight(.bold)
+                .font(.system(size: 18))
+            
+            VStack() {
+                Text("Dog Classification & Localization")
+                    .font(.custom("TrebuchetMS", size: 30))
+                    .fontWeight(.black)
+                    .foregroundColor(Color("AccentColor"))
+                    .multilineTextAlignment(.center)
+                    .padding(.top, 15)
+                
+                ZStack {
+                    // placeholder logo
+                    Image(systemName:"pawprint.fill")
+                        .font(.custom("TrebuchetMS", size: 24))
+                        .fontWeight(.regular)
+                        .foregroundColor(Color("titleColor").opacity(0.8))
+//                        .padding(.top, 1)
+                    Image(systemName: "pawprint")
+                        .font(.custom("TrebuchetMS", size: 24))
+                        .fontWeight(.regular)
+                        .foregroundColor(Color("AccentColor"))
+//                        .padding(.top, 1)
+                }.padding(.vertical, 1)
+                
+               
+                PhotoSwipeView()
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 10)
+
+                HStack{
+                    Text("Upload a photo of your dog and see their breed and a box around them, all done by my deep learning netwrok")
+                        .font(.custom("TrebuchetMS", size: 12))
+                        .foregroundColor(.accentColor)
+                        .multilineTextAlignment(.center)
+                        .padding(.horizontal, 40)
+                }
+
+                HStack{
+                    Image(systemName: "camera")
+                        .foregroundColor(.accentColor)
+                        .font(.system(size:18))
+                        .padding(.bottom, 2)
+                    
+                    Text("Take photo")
+                        .font(.custom("TrebuchetMS", size: 18))
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("AccentColor"))
+                        .padding(.leading, 5)
+                    
+                }.padding(.horizontal, 20)
+                 .padding(.vertical, 10)
+                 .background(Color("backgroundColor").opacity(0.3))
+                 .cornerRadius(20)
+                 .padding(.top, 40)
+                 .padding(.bottom, 3)
+                
+                HStack{
+                    Image(systemName: "square.and.arrow.up")
+                        .foregroundColor(.accentColor)
+                        .font(.system(size:17))
+                        .padding(.bottom, 2)
+                    
+                    Text("Choose photo")
+                        .font(.custom("TrebuchetMS", size: 18))
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("AccentColor"))
+                        .padding(.leading, 1)
+                    
+                }.padding(.horizontal, 16)
+                 .padding(.vertical, 8)
+                 .background(Color("backgroundColor").opacity(0.3))
+                 .cornerRadius(20)
+                 .padding(.top, 10)
+                
+            }.offset(y: 25)
+        }
+    }
+}
+
+struct HomePage_Previews: PreviewProvider {
+    static var previews: some View {
+        HomePage()
+    }
+}
---
+++ DogLocalizerSUI/PhotoSwipeView.swift
@@ -0,0 +1,67 @@
+import SwiftUI
+
+struct Photo: Identifiable {
+    var id = UUID()
+    var imageName: String
+    var description: String
+}
+
+struct CustomPageIndicatorView: View {
+    var currentIndex: Int
+    var pageCount: Int
+    
+    var body: some View {
+        HStack {
+            ForEach(0..<pageCount, id: \.self) { index in
+                Circle()
+                    .fill(index == currentIndex ? Color("AccentColor") : Color("AccentColor").opacity(0.5))
+                    .frame(width: 8, height: 8)
+            }
+        }
+        .padding(.bottom, 5)
+    }
+}
+
+
+struct PhotoSwipeView: View {
+    let photos: [Photo] = [
+        Photo(imageName: "photo1", description: "A photo of a dog from the Stanford Dogs dataset"),
+        Photo(imageName: "photo2", description: "That same photo, resized to fit the model requirements"),
+        Photo(imageName: "photo3", description: "The resized photo with the REAL bounding box"),
+        Photo(imageName: "photo4", description: "The photo with the REAL and PREDICTED \nbounding box and classification")
+    ]
+    @State private var selectedIndex = 0
+    
+    var body: some View {
+        VStack {
+            TabView(selection: $selectedIndex) {
+                ForEach(photos.indices, id: \.self) { index in
+                    VStack {
+                        Image(photos[index].imageName)
+                            .resizable()
+                            .scaledToFill()
+                        Text(photos[index].description)
+                            .multilineTextAlignment(.center)
+                            .padding(.top, 5)
+                            .padding(.horizontal, 20)
+                            .foregroundColor(Color("AccentColor"))
+                            .font(.custom("TrebuchetMS", size: 11))
+                            .fontWeight(.bold)
+                    }
+                    .tag(index)
+                }
+            }
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+            .frame(height: 350) // Adjust as needed
+            
+            CustomPageIndicatorView(currentIndex: selectedIndex, pageCount: photos.count)
+
+        }
+    }
+    
+    struct PhotoSwipeView_Previews: PreviewProvider {
+        static var previews: some View {
+            PhotoSwipeView()
+        }
+    }
+}
